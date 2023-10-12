423b51c21d17348fe59ed61ee1b06f6398b826be
Barcodew <ahmadaliakbar203@gmail.com>
2023-10-12 17:52:05 +0800

    Coin Change Greedy

---
+++ day - 4/CoinChangeGreedy.js
@@ -0,0 +1,31 @@
+function greedyCoinChange(coins, amount) {
+    // Menyortir koin dalam urutan menurun
+    coins.sort((a, b) => b - a);
+    
+    const change = {};
+    
+    for (let i = 0; i < coins.length; i++) {
+      const coin = coins[i];
+      while (amount >= coin) {
+        // Mengurangi nilai koin dari jumlah yang perlu diubah
+        amount -= coin;
+        
+        // Menambahkan koin ke daftar perubahan
+        if (change[coin]) {
+          change[coin]++;
+        } else {
+          change[coin] = 1;
+        }
+      }
+    }
+    
+    return change;
+  }
+  
+  const availableCoins = [25, 10, 5, 1];
+  const targetAmount = 15;
+  
+  const result = greedyCoinChange(availableCoins, targetAmount);
+  console.log("Perubahan koin yang diperlukan:");
+  console.log(result);
+  
