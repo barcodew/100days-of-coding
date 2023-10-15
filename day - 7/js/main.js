0de79d874d3f13c18f0f6e1d863b2194c67beba6
barcodew <ahmadaliakbar203@gmail.com>
2023-10-15 20:56:36 +0800

    Type Running

--- day - 7/js/main.js
+++ day - 7/js/main.js
@@ -1,4 +1,9 @@
-const ngetikApa = ["Halo, Selamat Datang", "Perkenalkan!", "Nama Saya","Achmad Ali Akbar"];
+const ngetikApa = [
+  "Halo, Selamat Datang",
+  "Perkenalkan!",
+  "Nama Saya",
+  "Achmad Ali Akbar",
+];
 const element = document.getElementById("ngetik-sendiri");
 let currentIndex = 0;
 
@@ -7,16 +12,19 @@ function gasNgetik() {
     const currentText = ngetikApa[currentIndex];
     const currentTextLength = currentText.length;
 
-    element.textContent = currentText.substring(0, element.textContent.length + 1);
+    element.textContent = currentText.substring(
+      0,
+      element.textContent.length + 1
+    );
 
     if (element.textContent.length === currentTextLength) {
-      setTimeout(hapusText, 200); // Tunggu 1 detik sebelum menghapus teks
+      setTimeout(hapusText, 200);
     } else {
-      setTimeout(gasNgetik, 100); // Tunggu 100 milidetik sebelum menambahkan karakter berikutnya
+      setTimeout(gasNgetik, 100);
     }
   } else {
-    currentIndex = 0; // Kembali ke awal jika semua teks sudah ditampilkan
-    setTimeout(gasNgetik, 1000); // Tunggu 1 detik sebelum mengulang dari awal
+    currentIndex = 0;
+    setTimeout(gasNgetik, 1000);
   }
 }
 
@@ -24,11 +32,11 @@ function hapusText() {
   const currentText = element.textContent;
   if (currentText.length > 0) {
     element.textContent = currentText.substring(0, currentText.length - 1);
-    setTimeout(hapusText, 100); // Tunggu 100 milidetik sebelum menghapus karakter sebelumnya
+    setTimeout(hapusText, 100);
   } else {
     currentIndex++;
-    setTimeout(gasNgetik, 100); // Tunggu 0.5 detik sebelum mengetik teks berikutnya
+    setTimeout(gasNgetik, 100);
   }
 }
 
-gasNgetik(); 
+gasNgetik();
