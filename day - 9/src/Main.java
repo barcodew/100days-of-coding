5386098cbe1553e2975facc85deaa8b6788fe988
Barcodew <ahmadaliakbar203@gmail.com>
2023-10-17 19:51:34 +0800

    Menampilkan Sejumlah n Baris

---
+++ day - 9/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n;
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            System.out.println("Masukkan Nilai n : ");
+            n = input.nextInt();
+
+            for (int i = 0; i < n; i++) {
+                System.out.println("INFORMATIKA");
+            }
+
+            System.out.println("Lanjut bang? (y/n) :");
+            String ulang = input.next();
+            if (ulang.toLowerCase().contains("n")) {
+                break;
+            }
+        }
+        System.out.println("Program Selesai, Have a nice day ^_^");
+    }
+}
