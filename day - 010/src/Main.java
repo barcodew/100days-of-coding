bed352a30a9a2576e15f35ae6a93011e2b0c5051
Achmad Ali Akbar <114000864+barcodew@users.noreply.github.com>
2023-10-18 12:34:40 +0800

    Menentukan Nilai MIN & MAX Pada 5 Variabel

--- day - 010/src/Main.java
+++ day - 010/src/Main.java
@@ -13,7 +13,7 @@ public class Main {
         System.out.print("Masukkan Nilai d : ");
         double d = input.nextDouble();
         System.out.print("Masukkan Nilai e : ");
-        double e = input.nextDouble();
+        double e = input.nextDouble(); 
 
         double min = Math.min(Math.min(Math.min(Math.min(a, b), c), d), e);
         double max = Math.max(Math.max(Math.max(Math.max(a, b), c), d), e);
